9306787da5956f9cb176cf8533c4932ef501f9d3
fatou dieng <dieng.fasou@gmail.com>
2025-04-06 13:02:54 +0200

    configuration multiEnvironnement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.staging';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'productivity-planner';
+  IsProductionEnvironnement = environment.production;
+  firebaseProjectId = environment.firebase.projectId; 
 }
